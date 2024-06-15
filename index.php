ec9b3c2f412f11ef6f8a1d9c3f5cdb56132a41c1
Greg French <gregfrench26@gmail.com>
2024-06-15 18:08:17 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,421 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Greg French is a software engineer in Guelph, ON Canada. He has a bachelor's degree in computer science from the University of Guelph.">
+
+    <title>Greg French - Software Engineer</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Fonts -->
+    <!--<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">-->
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+	<link href="css/animate.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
+	<link href="color/default.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+	<!-- Preloader -->
+	<div id="preloader">
+	  <div id="load"></div>
+	</div>
+
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <h1>GREG FRENCH</h1>
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#intro">Home</a></li>
+        <li><a href="#projects">Projects</a></li>
+		<li><a href="#skills">Skills</a></li>
+		<li><a href="#contact">Contact</a></li>
+      </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+	<!-- Section: intro -->
+    <section id="intro" class="intro">
+
+		<div class="slogan">
+			<h2>GREG <span class="text_color">FRENCH</span> </h2>
+			<h4>SOFTWARE ENGINEER IN GUELPH, ON CANADA</h4>
+		</div>
+		<div class="page-scroll">
+			<a href="#projects" class="btn btn-circle">
+				<i class="fa fa-angle-double-down animated"></i>
+			</a>
+		</div>
+    </section>
+	<!-- /Section: intro -->
+
+	<!-- Section: projects -->
+  <section id="projects" class="home-section text-center">
+		<div class="heading-about">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="wow bounceInDown" data-wow-delay="0.4s">
+					<div class="section-heading">
+					<h2>Latest Projects</h2>
+					<i class="fa fa-2x fa-angle-down"></i>
+
+					</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="container">
+  		<div class="row">
+  			<div class="col-lg-2 col-lg-offset-5">
+  				<hr class="marginbot-50">
+  			</div>
+  		</div>
+      <div class="section-heading">
+        <h3>WEB</h3>
+      </div>
+      <div class="row">
+        <div class="col-sm-6">
+          <!--
+          <div class="section-heading">
+            <h3>WEB</h3>
+          </div>
+          -->
+          <div class="row">
+            <div class="col-xs-12 col-sm-6">
+              <div class="wow bounceInUp" data-wow-delay="0.2s">
+                <div class="team boxed-grey">
+                  <div class="inner">
+                    <h5>Bootstrap Form Builder</h5>
+                    <div class="avatar"><img src="img/bootstrap-form-builder.png" alt="" class="img-responsive" /></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+              <div class="wow bounceInUp" data-wow-delay="0.5s">
+                <div class="team boxed-grey">
+                  <div class="inner">
+                    <h5>Gideon Golf Tournament</h5>
+                    <div class="avatar"><img src="img/gideon-golf-tournament.png" alt="" class="img-responsive" /></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <!--
+          <div class="section-heading">
+            <h3>WEB</h3>
+          </div>
+          -->
+          <div class="row">
+            <div class="col-xs-12 col-sm-6">
+              <div class="wow bounceInUp" data-wow-delay="0.8s">
+                <div class="team boxed-grey">
+                  <div class="inner">
+                    <h5>Jennes Boutique</h5>
+                    <div class="avatar"><img src="img/jennes-boutique.png" alt="" class="img-responsive" /></div>
+                  </div>
+                </div>
+      				</div>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+              <div class="wow bounceInUp" data-wow-delay="1s">
+              <div class="team boxed-grey">
+                <div class="inner">
+                  <h5>BGC</h5>
+                  <div class="avatar"><img src="img/bgc.png" alt="" class="img-responsive" /></div>
+                </div>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row new-projects">
+        <div class="col-sm-6">
+          <div class="section-heading">
+            <h3>VR / 3D Graphics</h3>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-6">
+              <div class="wow bounceInUp" data-wow-delay="0.2s">
+                <div class="team boxed-grey">
+                  <div class="inner">
+                    <h5>VR Haunted House</h5>
+                    <div class="avatar"><img src="img/haunted-house.png" alt="" class="img-responsive" /></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+              <div class="wow bounceInUp" data-wow-delay="0.5s">
+                <div class="team boxed-grey">
+                  <div class="inner">
+                    <h5>Unity Procedural Stair Builder</h5>
+                    <div class="avatar"><img src="img/procedural-stair-builder.png" alt="" class="img-responsive" /></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="section-heading">
+            <h3>Machine Learning</h3>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-6">
+              <div class="wow bounceInUp" data-wow-delay="0.8s">
+                <div class="team boxed-grey">
+                  <div class="inner">
+                    <h5>Gender Classification</h5>
+                    <div class="avatar"><img src="img/gender-classification.png" alt="" class="img-responsive" /></div>
+                  </div>
+                </div>
+      				</div>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+              <div class="wow bounceInUp" data-wow-delay="1s">
+                <div class="team boxed-grey">
+                  <div class="inner">
+                    <h5>Image Super Resolution</h5>
+                    <div class="avatar"><img src="img/super-resolution.png" alt="" class="img-responsive" /></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+	<!-- /Section: about -->
+
+
+	<!-- Section: skills -->
+    <section id="skills" class="home-section text-center bg-gray">
+
+		<div class="heading-about">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="wow bounceInDown" data-wow-delay="0.4s">
+					<div class="section-heading">
+					<h2>Skills</h2>
+					<i class="fa fa-2x fa-angle-down"></i>
+
+					</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="container">
+		<div class="row">
+			<div class="col-lg-2 col-lg-offset-5">
+				<hr class="marginbot-50">
+			</div>
+		</div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-3">
+				<div class="wow fadeInLeft" data-wow-delay="0.2s">
+                <div class="service-box">
+					<div class="service-icon">
+						<i class="fas fa-desktop" aria-hidden="true"></i>
+					</div>
+					<div class="service-desc">
+						<h5>WEB</h5>
+						<p>HTML, CSS, JavaScript, jQuery, VueJS, PHP, Laravel</p>
+					</div>
+                </div>
+				</div>
+            </div>
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				<div class="wow fadeInUp" data-wow-delay="0.2s">
+                <div class="service-box">
+					<div class="service-icon">
+            <i class="fas fa-mobile" aria-hidden="true"></i>
+					</div>
+					<div class="service-desc">
+						<h5>MOBILE</h5>
+						<p>Android, Java, React Native, IOS</p>
+					</div>
+                </div>
+				</div>
+            </div>
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				<div class="wow fadeInUp" data-wow-delay="0.2s">
+          <div class="service-box">
+  					<div class="service-icon">
+              <i class="fas fa-vr-cardboard"></i>
+  					</div>
+  					<div class="service-desc">
+  						<h5>VR / 3D Graphics</h5>
+  						<p>Unity, C#</p>
+  					</div>
+          </div>
+				</div>
+      </div>
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				<div class="wow fadeInRight" data-wow-delay="0.2s">
+          <div class="service-box">
+  					<div class="service-icon">
+              <i class="fas fa-robot"></i>
+  					</div>
+  					<div class="service-desc">
+  						<h5>Machine Learning</h5>
+  						<p>Tensorflow, Python</p>
+  					</div>
+          </div>
+				</div>
+      </div>
+    </div>
+		</div>
+	</section>
+	<!-- /Section: services -->
+
+
+
+
+	<!-- Section: contact -->
+    <section id="contact" class="home-section text-center">
+		<div class="heading-contact">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="wow bounceInDown" data-wow-delay="0.4s">
+					<div class="section-heading">
+					<h2>Get in touch</h2>
+					<i class="fa fa-2x fa-angle-down"></i>
+
+					</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="container">
+
+		<div class="row">
+			<div class="col-lg-2 col-lg-offset-5">
+				<hr class="marginbot-50">
+			</div>
+		</div>
+    <div class="row">
+      <div class="col-lg-2">
+      &nbsp;
+      </div>
+        <div class="col-lg-8">
+            <div class="boxed-grey">
+                <form id="contact-form" action="contact.php" method="post">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="name">
+                                Name</label>
+                            <input type="text" name="name" class="form-control" id="name" placeholder="Enter name" required="required" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">
+                                Email Address</label>
+                            <div class="input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                                </span>
+                                <input type="email" name="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
+                        </div>
+                        <div class="form-group">
+                            <label for="subject">
+                                Subject</label>
+                            <input id="subject" type="text" name="subject" class="form-control" required="required" />
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="name">
+                                Message</label>
+                            <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
+                                placeholder="Message"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-skin pull-right" id="btnContactUs">
+                            Send Message</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+
+		<div class="col-lg-2">
+			&nbsp;
+		</div>
+    </div>
+
+		</div>
+	</section>
+	<!-- /Section: contact -->
+
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-lg-12">
+					<div class="wow shake" data-wow-delay="0.4s">
+					<div class="page-scroll marginbot-30">
+						<a href="#intro" id="totop" class="btn btn-circle">
+							<i class="fa fa-angle-double-up animated"></i>
+						</a>
+					</div>
+					</div>
+					<p>&copy;Copyright <?php echo date("Y"); ?>. All rights reserved.</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+    <!-- Core JavaScript Files -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+  	<script src="js/jquery.scrollTo.js"></script>
+  	<script src="js/wow.min.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="js/custom.js"></script>
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-98494758-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+
+</html>
