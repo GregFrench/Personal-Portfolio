126c7aee2ad48f187a3ccd735c12ce340602b327
Greg French <gregfrench26@gmail.com>
2024-06-19 23:22:52 -0400

    Updated analytics

--- index.php
+++ index.php
@@ -203,7 +203,7 @@
               <div class="wow bounceInUp" data-wow-delay="1s">
                 <div class="team boxed-grey">
                   <div class="inner">
-                    <h5>Easy 21 (Blackjack RL solver)</h5>
+                    <h5>Easy 21 (Blackjack AI solver)</h5>
                     <div class="avatar"><img src="img/blackjack.jpg" alt="" class="img-responsive" /></div>
                   </div>
                 </div>
@@ -407,16 +407,15 @@
     <script src="js/custom.js"></script>
 
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-DKM48CHRKZ"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-  ga('create', 'UA-98494758-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
+    gtag('config', 'G-DKM48CHRKZ');
+  </script>
 
 </body>
 
